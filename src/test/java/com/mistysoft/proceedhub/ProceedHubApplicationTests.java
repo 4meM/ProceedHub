4e6e2bcd60283d8a8b7264697eeb238770a7a1a7
4meM <eramosq@unsa.edu.pe>
2025-07-17 13:40:50 -0500

    Debug: forzar a imprmir variables

--- src/test/java/com/mistysoft/proceedhub/ProceedHubApplicationTests.java
+++ src/test/java/com/mistysoft/proceedhub/ProceedHubApplicationTests.java
@@ -1,9 +1,7 @@
 package com.mistysoft.proceedhub;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ProceedHubApplicationTests {
 
     @Test
